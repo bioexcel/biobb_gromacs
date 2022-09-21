63df5d3e79449bffe2b71defd5bf274d9e413eef
Pau Andrio <andriopau@gmail.com>
2022-09-21 17:28:07 +0200

    Skipping solvate docker

--- biobb_gromacs/test/unitests/test_gromacs/test_solvate_docker.py
+++ biobb_gromacs/test/unitests/test_gromacs/test_solvate_docker.py
@@ -1,15 +1,18 @@
 from biobb_common.tools import test_fixtures as fx
 from biobb_gromacs.gromacs.solvate import solvate
+import pytest
 
 
-class TestSolvateDocker():
+class TestSolvateDocker:
     def setup_class(self):
         fx.test_setup(self, 'solvate_docker')
 
     def teardown_class(self):
-        pass
-        #fx.test_teardown(self)
+        # pass
+        fx.test_teardown(self)
 
+    @pytest.mark.skip(reason="Unable to overwrite topology file."
+                             "https://github.com/bioexcel/biobb_gromacs/wiki/Docker-Inout-files-ie:-topology-file-in-gmx-solvate")
     def test_solvate(self):
         returncode = solvate(properties=self.properties, **self.paths)
         assert fx.not_empty(self.paths['output_gro_path'])
