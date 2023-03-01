f6caf34a44787a6126ad6ce0ab6e7f0f91fd437a
Pau Andrio <andriopau@gmail.com>
2023-03-01 18:02:38 +0100

    Github actions

--- biobb_gromacs/gromacs/grompp_mdrun.py
+++ biobb_gromacs/gromacs/grompp_mdrun.py
@@ -149,7 +149,7 @@ class GromppMdrun(BiobbObject):
             fu.log(f'MDRun return code: {mdrun_return_code}', self.out_log, self.global_log)
         else:
             return 1
-        
+
         # Remove temporal files
         self.tmp_files.extend([self.stage_io_dict.get("unique_dir"), Path(self.output_tpr_path).parent])
         self.remove_tmp_files()
