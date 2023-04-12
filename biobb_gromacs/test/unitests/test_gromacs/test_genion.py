3593161511bea75e1552c2c3ad8dd170456b588c
Pau Andrio <andriopau@gmail.com>
2023-04-12 20:00:22 +0200

    test_genion: Change equal by compare_size

--- biobb_gromacs/test/unitests/test_gromacs/test_genion.py
+++ biobb_gromacs/test/unitests/test_gromacs/test_genion.py
@@ -1,6 +1,6 @@
 from biobb_common.tools import test_fixtures as fx
 from biobb_gromacs.gromacs.genion import genion
-import platform
+# import platform
 
 
 class TestGenion:
@@ -15,7 +15,9 @@ class TestGenion:
         returncode = genion(properties=self.properties, **self.paths)
         assert fx.not_empty(self.paths['output_gro_path'])
         assert fx.not_empty(self.paths['output_top_zip_path'])
+        assert fx.compare_size(self.paths['output_gro_path'], self.paths['ref_output_gro_path'])
+        assert fx.compare_size(self.paths['output_top_zip_path'], self.paths['ref_output_top_zip_path'])
         assert fx.exe_success(returncode)
-        if platform.system() == 'Darwin':
-            assert fx.equal(self.paths['output_gro_path'], self.paths['ref_output_gro_path'])
-            assert fx.equal(self.paths['output_top_zip_path'], self.paths['ref_output_top_zip_path'])
+        # if platform.system() == 'Darwin':
+        #    assert fx.equal(self.paths['output_gro_path'], self.paths['ref_output_gro_path'])
+        #    assert fx.equal(self.paths['output_top_zip_path'], self.paths['ref_output_top_zip_path'])
