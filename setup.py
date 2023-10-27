d6a898d96d43accff0e2fd17ce77caf2c59a51ca
gbayarri <genis.bayarri@irbbarcelona.org>
2023-10-27 15:41:35 +0200

    README

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     keywords="Bioinformatics Workflows BioExcel Compatibility",
     url="https://github.com/bioexcel/biobb_gromacs",
     project_urls={
-        "Documentation": "http://biobb_gromacs.readthedocs.io/en/latest/",
+        "Documentation": "http://biobb-gromacs.readthedocs.io/en/latest/",
         "Bioexcel": "https://bioexcel.eu/"
     },
     packages=setuptools.find_packages(exclude=['docs', 'test']),
