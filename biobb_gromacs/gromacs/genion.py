762b3a6fcad3b2ad3238212c1190470ce2245b80
Rubén Chaves <ruben.chaves@irbbarcelona.org>
2025-06-23 15:11:42 +0200

    fix genion concentration default #21

--- biobb_gromacs/gromacs/genion.py
+++ biobb_gromacs/gromacs/genion.py
@@ -30,7 +30,7 @@ class Genion(BiobbObject):
         properties (dict - Python dictionary object containing the tool parameters, not input/output files):
             * **replaced_group** (*str*) - ("SOL") Group of molecules that will be replaced by the solvent.
             * **neutral** (*bool*) - (False) Neutralize the charge of the system.
-            * **concentration** (*float*) - (0.05) [0~10|0.01] Concentration of the ions in (mol/liter).
+            * **concentration** (*float*) - (0.0) [0~10|0.01] Concentration of the ions in (mol/liter).
             * **seed** (*int*) - (1993) Seed for random number generator.
             * **gmx_lib** (*str*) - (None) Path set GROMACS GMXLIB environment variable.
             * **binary_path** (*str*) - ("gmx") Path to the GROMACS executable binary.
@@ -99,7 +99,7 @@ class Genion(BiobbObject):
         )  # Not in documentation for clarity
         self.replaced_group = properties.get("replaced_group", "SOL")
         self.neutral = properties.get("neutral", False)
-        self.concentration = properties.get("concentration", 0.05)
+        self.concentration = properties.get("concentration", 0.0)
         self.seed = properties.get("seed", 1993)
 
         # Properties common in all GROMACS BB
