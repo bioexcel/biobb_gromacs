d7e582a5d33a334a4cf9864b615ff43a61ed58e5
Rubén Chaves <ruben.chaves@irbbarcelona.org>
2025-06-25 11:57:02 +0200

    Update Python version classifiers in setup.py

--- setup.py
+++ setup.py
@@ -41,6 +41,9 @@ setuptools.setup(
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: POSIX",
