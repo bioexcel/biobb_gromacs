5c429081aeaae513b61bc273aec2e6c67ff15dce
Adam Hospital Gasch <hospital@OROZCO67-2.local>
2025-07-30 18:05:08 +0200

    fixed make_ndx stdin bug

--- biobb_gromacs/gromacs/make_ndx.py
+++ biobb_gromacs/gromacs/make_ndx.py
@@ -95,6 +95,7 @@ class MakeNdx(BiobbObject):
         if self.check_restart():
             return 0
 
+        self.selection = self.selection.replace("\\n", "\n")
         self.io_dict['in']['stdin_file_path'] = fu.create_stdin_file(f'{self.selection}\nq\n')
         self.stage_files()
 
