2b4296b75993ca4b401f12f31cc0d2518f2678a9
PabloNA97 <p.navarro.acero@gmail.com>
2025-10-19 13:54:09 +0200

    fix: capture renamed output files due to use of noappend in mdrun

--- biobb_gromacs/gromacs/mdrun.py
+++ biobb_gromacs/gromacs/mdrun.py
@@ -226,6 +226,58 @@ class Mdrun(BiobbObject):
         self.check_arguments(output_files_created=True, raise_exception=False)
         return self.return_code
 
+    def copy_to_host(self):
+        """
+        Updates the path to the original output files in the sandbox,
+        to catch changes due to noappend restart.
+
+        GROMACS mdrun will change the output file names from md.gro to md.part0001.gro
+        if the noappend flag is used.
+        """
+        import pathlib
+        
+        def capture_part_pattern(filename):
+            """
+            Captures the 'part' pattern followed by digits from a string.
+            """
+            import re
+            pattern = r'part\d+'
+            
+            match = re.search(pattern, filename)
+            if match:
+                return match.group(0)
+            else:
+                return None
+
+        if self.noappend:
+            # List files in the staging directory
+            staging_path = self.stage_io_dict["unique_dir"]
+            files_in_staging = list(pathlib.Path(staging_path).glob('*'))
+
+            # Find the part000x pattern in the output files
+            for file in files_in_staging:
+                part_pattern = capture_part_pattern(file.name)
+                if part_pattern:
+                    break
+
+            # Update expected output files
+            for file_ref, stage_file_path in self.stage_io_dict["out"].items():
+                if stage_file_path:
+                    # Find the parent and the file name in the sandbox
+                    parent_path = pathlib.Path(stage_file_path).parent
+                    file_stem = pathlib.Path(stage_file_path).stem
+                    file_suffix = pathlib.Path(stage_file_path).suffix
+                    
+                    # Rename all output files except checkpoint files
+                    if file_suffix != '.cpt':
+                        # Create the new file name with the part pattern
+                        if part_pattern:
+                            new_file_name = f"{file_stem}.{part_pattern}{file_suffix}"
+                            new_file_path = parent_path / new_file_name
+                            # Update the stage_io_dict with the new file path
+                            self.stage_io_dict["out"][file_ref] = str(new_file_path)
+
+        return super().copy_to_host()
 
 def mdrun(input_tpr_path: str, output_gro_path: str, output_edr_path: str,
           output_log_path: str, output_trr_path: Optional[str] = None, input_cpt_path: Optional[str] = None,
